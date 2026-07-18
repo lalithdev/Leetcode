beb426dbe9f20f596b8851395f79f1c0f87c1576
Lalith Aditya Singuparapu <2400031810@kluniversity.in>
2026-07-18 15:59:13 +0530

    Time: 0 ms (100%), Space: 43.2 MB (17.14%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words=s.trim().split("\\s+");
-        return words[words.length-1].length();
+        int lastLen=0, i=s.length()-1;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' '){
+            lastLen++;
+            i--;
+        }
+        return lastLen;
     }
 }
